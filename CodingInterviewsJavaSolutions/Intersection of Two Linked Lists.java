bee087b515dd5c1628ff3c008726e0ffe340d641
Robert Bogan <rajbogan@outlook.com>
2024-03-29 23:05:02 -0700

    Update Intersection of Two Linked Lists.java

--- CodingInterviewsJavaSolutions/Intersection of Two Linked Lists.java
+++ CodingInterviewsJavaSolutions/Intersection of Two Linked Lists.java
@@ -1,17 +1,22 @@
-class Solution:
-    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        copy_A = headA
-        copy_B = headB
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode copy_A = headA;
+        ListNode copy_B = headB;
         
-        while copy_A != copy_B:
-            if copy_A is None:
-                copy_A = headB
-            else:
-                copy_A = copy_A.next
+        while (copy_A != copy_B) {
+            if (copy_A == null) {
+                copy_A = headB;
+            } else {
+                copy_A = copy_A.next;
+            }
             
-            if copy_B is None:
-                copy_B = headA
-            else:
-                copy_B = copy_B.next
+            if (copy_B == null) {
+                copy_B = headA;
+            } else {
+                copy_B = copy_B.next;
+            }
+        }
         
-        return copy_A
+        return copy_A;
+    }
+}
