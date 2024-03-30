c9c8fa1f38a6944870484cbae4f8c0a784e5e6c7
Robert Bogan <rajbogan@outlook.com>
2024-03-29 23:46:15 -0700

    Update Sliding Puzzle.cpp

--- AlgorithmsPackCPP/Sliding Puzzle.cpp
+++ AlgorithmsPackCPP/Sliding Puzzle.cpp
@@ -1,21 +1,22 @@
 class Solution {
     private:
-    string goal = "123450";  //We'll be converting our function into a string!
-    vector<vector<int>>dir{{1,3},{0,2,4},{1,5},{0,4},{1,3,5},{2,4}};   //This works in a similar way to flattening the 2D array.
-    //Index 0 has neighbours 1/3, 1 has neighbours 0/2/4, and so on.
+    string goal = "123450"; 
+    vector<vector<int>>dir{{1,3},{0,2,4},{1,5},{0,4},{1,3,5},{2,4}};   
+//This works in a similar way to flattening the 2D array.
+//Index 0 has neighbours 1/3, 1 has neighbours 0/2/4, and so on.
 public:
     int slidingPuzzle(vector<vector<int>>& board) {
         string start = "";  
         for(int i=0; i < board.size(); i++){
             for(int j = 0; j < board[0].size(); j++){
-                start+=board[i][j]+'0';   //We begin by building our start string!
+                start+=board[i][j]+'0'; 
             }
         }
         
-        if(start==goal)   //If we have an instant match, 0 steps are needed!
+        if(start==goal) 
             return 0;
         
-        unordered_set<string>visited;   //These are fairly standard things to consider
+        unordered_set<string>visited;
         queue<string>q;
         q.push(start);
         visited.insert(start);
@@ -24,19 +25,19 @@ public:
             while(sz--){
                 string cur = q.front();
                 q.pop();
-                int idx1 = cur.find('0');  //This is the first 'unusual' thing.  Look for the index of '0' in the string
-                for(int idx2:dir[idx1]){   //Iterate through the index, performing swaps, and adding any uncovered strings into the queue!
-                    swap(cur[idx1],cur[idx2]);   //Perform the 'swap' of 0 from the current index to neighbouring indices
-                    if(cur==goal)   //If we make the goal string "123450", we return level+1
+                int idx1 = cur.find('0');
+                for(int idx2:dir[idx1]){ 
+                    swap(cur[idx1],cur[idx2]);  
+                    if(cur==goal) 
                         return level+1;
                     
-                    if(visited.insert(cur).second)   //Otherwise, we insert the 'cur' combination into the visited unSet,
-                        q.push(cur);  //AND, if insert(cur).second ;), add the cur combination to the queue!
+                    if(visited.insert(cur).second) 
+                        q.push(cur); 
                     
-                    swap(cur[idx1],cur[idx2]);    //Don't forget to swap back
+                    swap(cur[idx1],cur[idx2]); 
                 }
             }
         }
-        return -1;   //And double don't forget to return -1 if this proves impossible!
+        return -1;
     }
 };
