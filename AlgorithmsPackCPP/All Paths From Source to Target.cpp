fa18b79290acf6bd91fe3bd67a29d5b2b728b5cc
Robert Bogan <rajbogan@outlook.com>
2024-03-29 23:18:05 -0700

    Update All Paths From Source to Target.cpp

--- AlgorithmsPackCPP/All Paths From Source to Target.cpp
+++ AlgorithmsPackCPP/All Paths From Source to Target.cpp
@@ -1,26 +1,3 @@
-/*
-The backtracking solution is not so difficult to follow.
-Firstly, we know that we want to return all possible paths from 0 to n-1,
-necessitating both a vector<vector<int>>ANSWER and vector<int>CUR
-When we reach n-1, we will push CUR into the ANSWER!
-So, how do we work all this out?
-
-Firstly, create ANSWER and CUR, and then push 0 (the first node of any solved path) into cur
-After that, we use the initial graph, ANSWER, CUR, and the START and END variables,
-calling them recursively using backtracking to try and find a valid path (and add that path to our answer)
-The START is 0, the END is n-1!
-
-The backtracking function uses the initial graph, a START (which is initially 0), and END (ALWAYS n-1), and the ANSWER and CUR
-If we're at a point where START==END, when we have found a valid path, and can push CUR into ANS, returning
-
-Otherwise, we're going to have a look at the lists currently held at the INDEX we have.
-The INDEX is 'START'
-So, we iterate through our graph, such that:
-for(int i = 0; i < graph[start].size(); i++)
-We will ADD the graph[start][i] to CUR, then call the backtrack function recursively,
-then POP_BACK the latest added node
-*/
-
 class Solution {
 public:
     void backtrack(vector<vector<int>>&graph, int start, int end, vector<vector<int>>&ans, vector<int>&cur){
@@ -44,3 +21,26 @@ public:
         return ans;
     }
 };
+
+/*
+The backtracking solution is not so difficult to follow.
+Firstly, we know that we want to return all possible paths from 0 to n-1,
+necessitating both a vector<vector<int>>ANSWER and vector<int>CUR
+When we reach n-1, we will push CUR into the ANSWER!
+So, how do we work all this out?
+
+Firstly, create ANSWER and CUR, and then push 0 (the first node of any solved path) into cur
+After that, we use the initial graph, ANSWER, CUR, and the START and END variables,
+calling them recursively using backtracking to try and find a valid path (and add that path to our answer)
+The START is 0, the END is n-1!
+
+The backtracking function uses the initial graph, a START (which is initially 0), and END (ALWAYS n-1), and the ANSWER and CUR
+If we're at a point where START==END, when we have found a valid path, and can push CUR into ANS, returning
+
+Otherwise, we're going to have a look at the lists currently held at the INDEX we have.
+The INDEX is 'START'
+So, we iterate through our graph, such that:
+for(int i = 0; i < graph[start].size(); i++)
+We will ADD the graph[start][i] to CUR, then call the backtrack function recursively,
+then POP_BACK the latest added node
+*/
