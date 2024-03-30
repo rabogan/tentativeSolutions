5673f3cdcd209cac30228c910a0722df4d5bde67
Robert Bogan <rajbogan@outlook.com>
2024-03-30 00:50:11 -0700

    Update Diameter of Binary Tree.cpp

--- CodingInterviewsPackSolutionsCPP/Diameter of Binary Tree.cpp
+++ CodingInterviewsPackSolutionsCPP/Diameter of Binary Tree.cpp
@@ -23,17 +23,22 @@ public:
 };
 
 /*
-TO solve the Binary Tree diameter problem, first understand what it means!
-The diameter of a BT is simply the longest path in a Binary Tree
+TO solve the Binary Tree diameter problem, first understand 
+what it means!  The diameter of a BT is simply the longest 
+path in a Binary Tree
 
 1)  If there's no root to our tree, simply return 0
-2)  We'll use a global variable (max_diameter) to allow easy updating of our eventual result
+2)  We'll use a global variable (max_diameter) to allow 
+easy updating of our eventual result
 3)  To solve this, we will simply call the HEIGHT function
-You should be familiar with the height function (for BTs) already
-4)  Within our specialized height function call, we will continually update the maxDiameter
-4b) Whereby maxDiameter = max(maxDiameter,leftHeight + rightHeight)
-5)  Otherwise, our height function operates as standard, with recursive calls left and right,
-returning 1+the maximum of the left and right subtrees
+You should be familiar with the height function already
+4)  Within our specialized height function call, we will 
+continually update the maxDiameter
+4b) maxDiameter = max(maxDiameter,leftHeight + rightHeight)
+5)  Otherwise, our height function operates as standard, 
+with recursive calls left and right, returning 1+the 
+maximum of the left and right subtrees
 
-Essentially, we update our maxDiamter variable based on the left and right subtrees
+Essentially, we update our maxDiamter variable based 
+on the left and right subtrees
 */
