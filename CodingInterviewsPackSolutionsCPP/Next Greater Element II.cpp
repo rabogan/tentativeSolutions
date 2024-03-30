4c93b6383604b4bbd37584244f1d99d3c8a2f237
Robert Bogan <rajbogan@outlook.com>
2024-03-30 01:21:56 -0700

    Update Next Greater Element II.cpp

--- CodingInterviewsPackSolutionsCPP/Next Greater Element II.cpp
+++ CodingInterviewsPackSolutionsCPP/Next Greater Element II.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-	vector<int> nextGreaterElements(vector<int>& v) {
-		int n = v.size();
-		vector<int> res(n, -1);
-		stack<int> pos;
-        for (int i = 0; i < 2 * n; ++i) {
+   vector<int> nextGreaterElements(vector<int>& v) {
+	   int n = v.size();
+	   vector<int> res(n, -1);
+	   stack<int> pos;
+           for (int i = 0; i < 2 * n; ++i) {
 		while (!pos.empty() && v[i%n] > v[pos.top()]){
-			res[pos.top()] = v[i % n];
-			pos.pop();
+		   res[pos.top()] = v[i % n];
+		   pos.pop();
             	}
 		if (i < n)
-			pos.push(i);
+		   pos.push(i);
 		}
-		return res;
-	}
+	   return res;
+   }
 };
