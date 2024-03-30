20c0c8bba4559354da0d8556134597af7ca5ee88
Robert Bogan <rajbogan@outlook.com>
2024-03-29 22:56:50 -0700

    Update Largest Submatrix With Rearrangements.cpp

--- CodingInterviewsPackSolutionsCPP/Largest Submatrix With Rearrangements.cpp
+++ CodingInterviewsPackSolutionsCPP/Largest Submatrix With Rearrangements.cpp
@@ -1,53 +1,3 @@
-/*
-A convoluted, but successful approach!
-
-OUR GOAL:
-We want to get the maximum area of 1s possible, and can/must use rearrangement of the
-columns to do so.  Simply leaving the matrix as it is may not give the right solution
-
-OUR SOLUTION:
-1)  Take a look at how we can represent this matrix as a HISTOGRAM here:
-https://leetcode.com/submissions/detail/804786323/
-I used 'Next Smaller Element' rather than index (my mistake!)
-
-The TLDR:
-So, our rectangle area solution will find 2 vectors:
-a)  vector<int>next (for the NEXT SMALLER INDEX)
-b)  vector<int>prev (for the PREVIOUS SMALLER INDEX)
-c)  We iterate through our heights, and calculate an INITIAL AREA for a rectangle at height[i]
-d)  We maximize among these areas to get our result, returning (in my exemplar case) maxArea
-We will SLICE UP the matrix, going from the top row to the bottom row, calculating areas!
-
-2)  HOWEVER, we need to perform a special prefix sum calculation, so that the LOWER ROWS
-are well represented.  The details about this are similar to LeetCode 85 (HERE):
-https://leetcode.com/submissions/detail/804873331/
-The TLDR:
-a) Try EVERY row in our matrix, but as the base of a rectangle
-b) Step ZERO: don't forget to return 0 if the matrix is 0
-c)  Perform a special function, prefix_sum_column_variant, to change our integer matrix
-into a histogram - can you see how they add up?
-i.e.
-1 0 1 0 1   -------\    1 0 1 0 1
-1 0 1 1 1           \   2 0 2 1 2
-1 1 1 1 0            >  3 1 3 2 0
-1 0 0 1 1           /   4 0 0 3 1
-0 0 1 0 0   -------/    0 0 1 0 2
-
-
-3) BUT, we are allowed AND MUST sort out our rows, row by row, each time
-This will coagulate all our largest values to the RIGHT HAND SIDE...e.g.
-OUR PREFIX SUM ------>  OUR REARRANGED MATRIX
-1 0 1 0 1   -------\    0 0 1 1 1
-2 0 2 1 2           \   0 1 2 2 2
-3 1 3 2 0            >  0 1 2 3 3
-4 0 0 3 1           /   0 0 1 3 4
-0 0 1 0 2   -------/    0 0 0 1 2
-NOW, we send EACH ROW in the histogram as our VECTOR,
-and insert it into the largestRectangleArea functions
-Maximizing the area calculation should be relatively easy!
-Can you visually observe our answer here??? 12!!! (4*3)--->plus row number 3 (0,1,2,3,4) into the functions to see why!
-*/
-
 class Solution {
 public:
     void prefixSumCol(vector<vector<int>>&matrix){
@@ -111,3 +61,53 @@ public:
         return area;
     }
 };
+
+/*
+A convoluted, but successful approach!
+
+OUR GOAL:
+We want to get the maximum area of 1s possible, and can/must use rearrangement of the
+columns to do so.  Simply leaving the matrix as it is may not give the right solution
+
+OUR SOLUTION:
+1)  Take a look at how we can represent this matrix as a HISTOGRAM here:
+https://leetcode.com/submissions/detail/804786323/
+I used 'Next Smaller Element' rather than index (my mistake!)
+
+The TLDR:
+So, our rectangle area solution will find 2 vectors:
+a)  vector<int>next (for the NEXT SMALLER INDEX)
+b)  vector<int>prev (for the PREVIOUS SMALLER INDEX)
+c)  We iterate through our heights, and calculate an INITIAL AREA for a rectangle at height[i]
+d)  We maximize among these areas to get our result, returning (in my exemplar case) maxArea
+We will SLICE UP the matrix, going from the top row to the bottom row, calculating areas!
+
+2)  HOWEVER, we need to perform a special prefix sum calculation, so that the LOWER ROWS
+are well represented.  The details about this are similar to LeetCode 85 (HERE):
+https://leetcode.com/submissions/detail/804873331/
+The TLDR:
+a) Try EVERY row in our matrix, but as the base of a rectangle
+b) Step ZERO: don't forget to return 0 if the matrix is 0
+c)  Perform a special function, prefix_sum_column_variant, to change our integer matrix
+into a histogram - can you see how they add up?
+i.e.
+1 0 1 0 1   -------\    1 0 1 0 1
+1 0 1 1 1           \   2 0 2 1 2
+1 1 1 1 0            >  3 1 3 2 0
+1 0 0 1 1           /   4 0 0 3 1
+0 0 1 0 0   -------/    0 0 1 0 2
+
+
+3) BUT, we are allowed AND MUST sort out our rows, row by row, each time
+This will coagulate all our largest values to the RIGHT HAND SIDE...e.g.
+OUR PREFIX SUM ------>  OUR REARRANGED MATRIX
+1 0 1 0 1   -------\    0 0 1 1 1
+2 0 2 1 2           \   0 1 2 2 2
+3 1 3 2 0            >  0 1 2 3 3
+4 0 0 3 1           /   0 0 1 3 4
+0 0 1 0 2   -------/    0 0 0 1 2
+NOW, we send EACH ROW in the histogram as our VECTOR,
+and insert it into the largestRectangleArea functions
+Maximizing the area calculation should be relatively easy!
+Can you visually observe our answer here??? 12!!! (4*3)--->plus row number 3 (0,1,2,3,4) into the functions to see why!
+*/
