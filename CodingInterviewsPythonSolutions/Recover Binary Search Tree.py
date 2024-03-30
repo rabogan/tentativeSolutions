fb495c465fc4243c96df0e3ab399a74576dc1854
Robert Bogan <rajbogan@outlook.com>
2024-03-29 22:51:29 -0700

    Update Recover Binary Search Tree.py

--- CodingInterviewsPythonSolutions/Recover Binary Search Tree.py
+++ CodingInterviewsPythonSolutions/Recover Binary Search Tree.py
@@ -1,10 +1,3 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-
 class Solution:
     def recoverTree(self, root: Optional[TreeNode]) -> None:
         """
@@ -35,3 +28,10 @@ class Solution:
 
         traverse(root)
         mn.val, mx.val = mx.val, mn.val
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
