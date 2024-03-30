5daa9a2e4aa5c4a19adf0e94eb00351b62c444fe
Robert Bogan <rajbogan@outlook.com>
2024-03-29 22:54:49 -0700

    Update Exclusive Time of Functions.cpp

--- CodingInterviewsPackSolutionsCPP/Exclusive Time of Functions.cpp
+++ CodingInterviewsPackSolutionsCPP/Exclusive Time of Functions.cpp
@@ -1,47 +1,3 @@
-/*
-SOLUTION and EXPLANATION:
-1)  Firstly, it's pretty clear that a STACK is needed in the solution.   We need to define what an ENTRY would be!
-The stack entry would be defined using an ID, a start time, and a subcall time
-Build the class for this, and don't forget about the initializers!
-
-2)  We have a bunch of 'log' entries.  We need a way of parsing these things.  To do so, we can use a parsing function!
-This quite simply:
-a)  Removes/replaces the colon.   We change the colon to a ' '
-b)  Uses an istringstream, where istringstream iss(str)
-c)  We split the string into THREE things:  an id, a boolean variable we'll call type, and time
-d)  The boolean variable simply asks:  is ("start"==type)?  We return {id,type=="start",time}
-
-3)  Next up, we immediate define our result within the exclusiveTime function!
-vector<int>result(n);
-stack<Entry>log_stack;
-In the style of 'love shack', we'll call the stack 'log_stack'
-To allow easier coding:   log_stack.push({0,0})  (id, startTime), remember?
-
-4)  We then iterate through the log of calls
-First of all, we need to parse out the calls.  It's EASY!
-vector<int>v = parse(logs[i]);
-int id = v[0];
-bool type = v[1];
-int time = v[2];
-
-5)  if(type) --->  If we have a start, then we know that we need to PUSH the entry into the stack.  SO:
-if(type)
-log_stack.push({id,time});
-
-6)  The else-condition is where it all happens!
-a)  We grab the entry from the top of the stack, and extract the ID and START TIME for that entry
-b)  We need to calculate a TOTAL (which is time-START TIME+1) and a variable EXCLUSIVE (which is TOTAL-e.subcallTime)
-bii)  In summary, we pop the top of the stack, and get an ID and time.  We then calculate a TOTAL and EXCLUSIVE
-c)  We then need to update subsequent stack entries.  We start by popping the top of the stack, calling this entry e_parent
-d)  We use TOTAL to update e_parent's subcall time entry:
-e_parent.subcallTime+=total;
-e)  We push e_parent back into the stack!   log_stack.push(e_parent)
-f)  EXCLUSIVE is for our eventual result vector!!!!
-result[id]+=exclusive;
-
-7)  Return the result!
-*/
-
 class Entry{
     public:
     int id{};
@@ -102,3 +58,47 @@ public:
         return result;
     }
 };
+
+/*
+SOLUTION and EXPLANATION:
+1)  Firstly, it's pretty clear that a STACK is needed in the solution.   We need to define what an ENTRY would be!
+The stack entry would be defined using an ID, a start time, and a subcall time
+Build the class for this, and don't forget about the initializers!
+
+2)  We have a bunch of 'log' entries.  We need a way of parsing these things.  To do so, we can use a parsing function!
+This quite simply:
+a)  Removes/replaces the colon.   We change the colon to a ' '
+b)  Uses an istringstream, where istringstream iss(str)
+c)  We split the string into THREE things:  an id, a boolean variable we'll call type, and time
+d)  The boolean variable simply asks:  is ("start"==type)?  We return {id,type=="start",time}
+
+3)  Next up, we immediate define our result within the exclusiveTime function!
+vector<int>result(n);
+stack<Entry>log_stack;
+In the style of 'love shack', we'll call the stack 'log_stack'
+To allow easier coding:   log_stack.push({0,0})  (id, startTime), remember?
+
+4)  We then iterate through the log of calls
+First of all, we need to parse out the calls.  It's EASY!
+vector<int>v = parse(logs[i]);
+int id = v[0];
+bool type = v[1];
+int time = v[2];
+
+5)  if(type) --->  If we have a start, then we know that we need to PUSH the entry into the stack.  SO:
+if(type)
+log_stack.push({id,time});
+
+6)  The else-condition is where it all happens!
+a)  We grab the entry from the top of the stack, and extract the ID and START TIME for that entry
+b)  We need to calculate a TOTAL (which is time-START TIME+1) and a variable EXCLUSIVE (which is TOTAL-e.subcallTime)
+bii)  In summary, we pop the top of the stack, and get an ID and time.  We then calculate a TOTAL and EXCLUSIVE
+c)  We then need to update subsequent stack entries.  We start by popping the top of the stack, calling this entry e_parent
+d)  We use TOTAL to update e_parent's subcall time entry:
+e_parent.subcallTime+=total;
+e)  We push e_parent back into the stack!   log_stack.push(e_parent)
+f)  EXCLUSIVE is for our eventual result vector!!!!
+result[id]+=exclusive;
+
+7)  Return the result!
+*/
