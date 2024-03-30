f89e7f7693c2ef1f4453a0882a122ca24731c798
Robert Bogan <rajbogan@outlook.com>
2024-03-29 23:48:34 -0700

    Update Strange Printer II.cpp

--- AlgorithmsPackCPP/Strange Printer II.cpp
+++ AlgorithmsPackCPP/Strange Printer II.cpp
@@ -5,14 +5,6 @@ class Solution {
         graph[from].push_back(to);
     }
 public:
-    /*Briefly about the top 3 functions:
-    1) The graph is almost boringly rudimentary
-    2) We want to return an 'ordering' vector; it will ???
-    3) The topSort function is as simple as usual.  There are two tricks
-    4) One is to push the 'i' (or front()) value of the queue into the result vector
-    5) The second is to use clear() on the ordering vector if the size is doesn't match the graph size
-    6) Otherwise, return the ordering vector
-    */
     vector<int>topSort(GRAPH&graph){
         int sz = graph.size();
         vector<int>incomingEdges(sz);
@@ -42,17 +34,7 @@ public:
             ordering.clear();
         return ordering;
     }
-    
-    /*
-    And now, for a diatribe about this function ;)
-    1)  Immediately, we have a need for a row number, a column number, and a maxColor (see the dimensions laid out by the problem)
-    2)  We get 4 vectors for the row and col range for EACH colour of the rectangle!  See how we initialize the max with -1, and min with rows/cols
-    3)  The first big loop: gets the color of the grid (after some 'easy loops', then iterates through the whole grid, and maximises/minimise the four boundaries for the colour of the rectangle being looked at!
-    4)  We then FINALLY create our graph, using...simply, the maxColor!
-    5)  Now, the second big loop!  Wow!   It culminates in ADDING an edge!  We do this using the graph, color, and eventual targetGrid[r][c]
-    5b)   The 3 loop iteration isn't that tricky, but you will need to be careful with your <= operators!
-    6) The 'clear()' trick in the topSort function becomes apparent!   return topSort(graph).size()>0!!!
-    */
+
     bool isPrintable(vector<vector<int>>& targetGrid) {
         int rows = targetGrid.size();
         int cols = targetGrid[0].size();
@@ -66,8 +48,8 @@ public:
         for(int r = 0; r < rows; r++){
             for(int c = 0; c < cols; c++){
                 int color = targetGrid[r][c];
-                rmax[color] = max(rmax[color],r);   //So, this maximises -1 vs the row (r)
-                rmin[color] = min(rmin[color],r);   //This minimises rows vs the row(r)
+                rmax[color] = max(rmax[color],r); 
+                rmin[color] = min(rmin[color],r); 
                 cmax[color] = max(cmax[color],c);
                 cmin[color] = min(cmin[color],c);
             }
@@ -86,3 +68,38 @@ public:
         return topSort(graph).size()>0;   
     }
 };
+
+/*Briefly about the top 3 functions:
+1) The graph is almost boringly rudimentary
+2) We want to return an 'ordering' vector; it will ???
+3) The topSort function is as simple as usual.  There are 
+two tricks
+4) One is to push the 'i' (or front()) value of the queue 
+into the result vector
+5) The second is to use clear() on the ordering vector 
+if the size is doesn't match the graph size
+6) Otherwise, return the ordering vector
+*/
+
+/*
+And now, for a diatribe about isPrintable:
+1)  Immediately, we have a need for a row number, a 
+column number, and a maxColor (see the dimensions laid 
+out by the problem)
+2)  We get 4 vectors for the row and col range for EACH 
+colour of the rectangle!  See how we initialize the max 
+with -1, and min with rows/cols
+3)  The first big loop: gets the color of the grid (after 
+some 'easy loops', then iterates through the whole grid, 
+and maximises/minimise the four boundaries for the colour 
+of the rectangle being looked at!
+4)  We then FINALLY create our graph, using...simply, 
+the maxColor!
+5)  Now, the second big loop!  Wow!   It culminates in 
+ADDING an edge!  We do this using the graph, color, and 
+eventual targetGrid[r][c]
+5b)   The 3 loop iteration isn't that tricky, but you 
+will need to be careful with your <= operators!
+6) The 'clear()' trick in the topSort function becomes 
+apparent!   return topSort(graph).size()>0!!!
+*/
