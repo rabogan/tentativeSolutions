1ebb929e42d4bf963d1337186ba2f5965035037d
Robert Bogan <rajbogan@outlook.com>
2024-03-29 23:21:37 -0700

    Update All Paths From Source to Target.cpp

--- AlgorithmsPackCPP/All Paths From Source to Target.cpp
+++ AlgorithmsPackCPP/All Paths From Source to Target.cpp
@@ -21,26 +21,33 @@ public:
         return ans;
     }
 };
-
 /*
 The backtracking solution is not so difficult to follow.
-Firstly, we know that we want to return all possible paths from 0 to n-1,
-necessitating both a vector<vector<int>>ANSWER and vector<int>CUR
+Firstly, we know that we want to return all possible 
+paths from 0 to n-1, necessitating both a 
+vector<vector<int>>ANSWER and vector<int>CUR.
 When we reach n-1, we will push CUR into the ANSWER!
 So, how do we work all this out?
 
-Firstly, create ANSWER and CUR, and then push 0 (the first node of any solved path) into cur
-After that, we use the initial graph, ANSWER, CUR, and the START and END variables,
-calling them recursively using backtracking to try and find a valid path (and add that path to our answer)
+Firstly, create ANSWER and CUR, and then push 0 
+(the first node of any solved path) into cur.
+After that, we use the initial graph, ANSWER, CUR, and 
+the START and END variables, calling them recursively 
+using backtracking to try and find a valid path (and 
+add that path to our answer)
 The START is 0, the END is n-1!
 
-The backtracking function uses the initial graph, a START (which is initially 0), and END (ALWAYS n-1), and the ANSWER and CUR
-If we're at a point where START==END, when we have found a valid path, and can push CUR into ANS, returning
+The backtracking function uses the initial graph, a 
+START (which is initially 0), and END (ALWAYS n-1), 
+and the ANSWER and CUR.  If we're at a point where 
+START==END, when we have found a valid path, and can 
+push CUR into ANS, returning
 
-Otherwise, we're going to have a look at the lists currently held at the INDEX we have.
-The INDEX is 'START'
+Otherwise, we're going to have a look at the lists 
+currently held at the INDEX we have. The INDEX is 'START'
 So, we iterate through our graph, such that:
 for(int i = 0; i < graph[start].size(); i++)
-We will ADD the graph[start][i] to CUR, then call the backtrack function recursively,
-then POP_BACK the latest added node
+We will ADD the graph[start][i] to CUR, then call the
+backtrack function recursively, then POP_BACK the 
+latest added node
 */
