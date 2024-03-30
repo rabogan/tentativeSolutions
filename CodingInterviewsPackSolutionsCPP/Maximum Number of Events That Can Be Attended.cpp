83687a20b7c998199410c0cacef4e0ea5eb034fa
Robert Bogan <rajbogan@outlook.com>
2024-03-29 22:58:47 -0700

    Update Maximum Number of Events That Can Be Attended.cpp

--- CodingInterviewsPackSolutionsCPP/Maximum Number of Events That Can Be Attended.cpp
+++ CodingInterviewsPackSolutionsCPP/Maximum Number of Events That Can Be Attended.cpp
@@ -1,30 +1,3 @@
-/*
-1a)  Sorting the events from day-start order is important!
-1b)  Get the number attended, the event itself, and the size of events
-Our minHeap will be used to use to store the 'end day' of our events 
-
-2)  We're iterating by day!  Event must be less than size; minHeap must have something in it!
-The heap will maintain only when the events will end...and all elements in the
-heap are active (i.e. started some time ago)
-We will get rid of events that end sooner compared to others
-
-3)  Remove all events that are already finished!
-Just look for minHeap elements LESS THAN the current day, and pop them
-With each 'day', we remove all the old events
-
-4)  Optimisation!  If our event is < size, and our minHeap has elements...
-...then change day to the max of day and events[event][0]
-We are simply JUMPING TO THE NEXT DAY (if our heap is empty and event < sz)
-INSTEAD OF simply iterating through a bunch of useless unused days!
-
-5)  We're iterating from the last added event
-While event < sz AND the current events's start day is <= day...
-...push events[event++][1] into the minHeap!
-Each event is ONLY added to the heap once!
-
-6)  Make sure you get the event ending first!
-*/
-
 class Solution {
 public:
     int maxEvents(vector<vector<int>>& events) {
@@ -53,3 +26,29 @@ public:
         return attended;
     }
 };
+/*
+1a)  Sorting the events from day-start order is important!
+1b)  Get the number attended, the event itself, and the size of events
+Our minHeap will be used to use to store the 'end day' of our events 
+
+2)  We're iterating by day!  Event must be less than size; minHeap must have something in it!
+The heap will maintain only when the events will end...and all elements in the
+heap are active (i.e. started some time ago)
+We will get rid of events that end sooner compared to others
+
+3)  Remove all events that are already finished!
+Just look for minHeap elements LESS THAN the current day, and pop them
+With each 'day', we remove all the old events
+
+4)  Optimisation!  If our event is < size, and our minHeap has elements...
+...then change day to the max of day and events[event][0]
+We are simply JUMPING TO THE NEXT DAY (if our heap is empty and event < sz)
+INSTEAD OF simply iterating through a bunch of useless unused days!
+
+5)  We're iterating from the last added event
+While event < sz AND the current events's start day is <= day...
+...push events[event++][1] into the minHeap!
+Each event is ONLY added to the heap once!
+
+6)  Make sure you get the event ending first!
+*/
