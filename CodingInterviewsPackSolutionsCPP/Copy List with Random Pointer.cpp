0325c5919cffc260d9c018d2732a5a911f6d3a9b
Robert Bogan <rajbogan@outlook.com>
2024-03-30 00:49:24 -0700

    Update Copy List with Random Pointer.cpp

--- CodingInterviewsPackSolutionsCPP/Copy List with Random Pointer.cpp
+++ CodingInterviewsPackSolutionsCPP/Copy List with Random Pointer.cpp
@@ -28,17 +28,21 @@ public:
 
 /*
 SOLUTION:
-The O(N) solution provided here is done with careful sketching out of the nodes!
+The O(N) solution provided here is done with careful 
+sketching out of the nodes!
 The basic plan:
-1)  Build copies of the nodes (new nodes), and have their 'random' pointers point to the
-same node in the original linked list
-2)  As you do this, use an unordered_map to keep a record of the ORIGINAL NODE TO COPY NODE
-This means (when we clear up where the random pointers will be pointing to) that we can easily
-adjust the random pointer, using something like:
-copyNode->random->next = myMap[copyNode->random->next];   The map should map the old node to the new one!
-3)  Don't forget that you'll need to maintain the 'next pointer' as normal in the Linked List too!
-
-
+1)  Build copies of the nodes (new nodes), and have their 
+'random' pointers point to the same node in the original 
+linked list
+2)  As you do this, use an unordered_map to keep a record 
+of the ORIGINAL NODE TO COPY NODE
+This means (when we clear up where the random pointers 
+will be pointing to) that we can easily adjust the random 
+pointer, using something like:
+copyNode->random->next = myMap[copyNode->random->next];   
+The map should map the old node to the new one!
+3)  Don't forget that you'll need to maintain the 
+'next pointer' as normal in the Linked List too!
 
 // Definition for a Node.
 class Node {
