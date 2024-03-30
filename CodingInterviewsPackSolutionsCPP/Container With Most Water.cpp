9db30512113f50633359463fa7134727587de1e0
Robert Bogan <rajbogan@outlook.com>
2024-03-30 00:48:30 -0700

    Update Container With Most Water.cpp

--- CodingInterviewsPackSolutionsCPP/Container With Most Water.cpp
+++ CodingInterviewsPackSolutionsCPP/Container With Most Water.cpp
@@ -18,12 +18,15 @@ public:
 
 /*
 The BF solution is very simple, but will TLE
-The approach is clearly about using 2 pointers, because
-Prefix/CumSum needs a minimum among the boundaries (not okay here)
-Sliding windows depend only on boundaries, and not at all on the body
+The approach is clearly about using 2 pointers, 
+because Prefix/CumSum needs a minimum among the 
+boundaries (not okay here)
+Sliding windows depend only on boundaries, and not 
+at all on the body
 
-Key to the 2-pointers approach is to move from
-the side with the LOWER value!  (Start from far-left and far-right) :)
+The key to the 2-pointers approach is to move from
+the side with the LOWER value!  (Start from far-left 
+and far-right) :)
 
 O(N) in time
 O(1) in memory
